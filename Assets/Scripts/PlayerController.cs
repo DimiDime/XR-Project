fc1ba4bb71e4d100ea4d27492a71981b59219b2d
Dimi051100 <dimitar_ilev@t-online.de>
2022-05-18 17:16:55 +0300

    MultiUseIntegration
    Added input handling by use of Zinnia actions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Zinnia.Action;
 
 public class PlayerController : MonoBehaviour
 {
+    public Vector2Action moveInput;
     public Rigidbody player; // Variable refrences to a Transform comp
     //public float speed = 1.0f;
     public float forceMultiplier = 10f;
@@ -21,11 +23,10 @@ public class PlayerController : MonoBehaviour
         // 3d Vector in space
         Vector3 moveDirection = Vector3.zero; // Direction of x axis
 
-        float horizontalInput = Input.GetAxis("Horizontal");
-        moveDirection += new Vector3(horizontalInput, 0, 0);
-
-        float verticalInput = Input.GetAxis("Vertical");
-        moveDirection += new Vector3(0, 0, verticalInput);
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        moveDirection += new Vector3(moveInput.Value.x, 0, 0);
+        //float verticalInput = Input.GetAxis("Vertical");
+            moveDirection += new Vector3(0, 0, moveInput.Value.y);
 
         //Vector3 moveDelta = moveDirection * (speed * Time.deltaTime); // direction * v * t = pos
 
@@ -37,3 +38,4 @@ public class PlayerController : MonoBehaviour
 
 // Notes
 // Unity serializes all public variables
+// Event-based input handling
